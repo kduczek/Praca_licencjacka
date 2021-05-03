7b6e40b7a2eeeb7474c15d6d5a9488ae596a0429
kduczek <duczek.krystian@gmail.com>
2021-05-03 21:50:01 +0200

    Simplified JBehave configuration

--- src/test/runners/JBehaveRunner.java
+++ src/test/runners/JBehaveRunner.java
@@ -1,6 +1,5 @@
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
-import org.jbehave.core.io.LoadFromClasspath;
 import org.jbehave.core.junit.JUnitStories;
 import org.jbehave.core.reporters.Format;
 import org.jbehave.core.reporters.StoryReporterBuilder;
@@ -18,7 +17,8 @@ public class JBehaveRunner extends JUnitStories {
 
     @Override
     public Configuration configuration() {
-        return new MostUsefulConfiguration().useStoryLoader(new LoadFromClasspath(getClass().getClassLoader())).useStoryReporterBuilder(new StoryReporterBuilder().withFormats(Format.STATS, Format.CONSOLE));
+        return new MostUsefulConfiguration()
+                .useStoryReporterBuilder(new StoryReporterBuilder().withFormats(Format.STATS, Format.CONSOLE));
     }
 
     @Override
